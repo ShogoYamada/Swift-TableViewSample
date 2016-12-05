8fe6d6811363fcd7f9708542a3a54bc4a62af0f4
山田尚吾 <jesus9387@outlook.com>
2016-12-05 21:29:57 +0900

    TableViewサンプル作成

--- TableVIew/ViewController.swift
+++ TableVIew/ViewController.swift
@@ -8,18 +8,53 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    ///Edit:テキストを入力する場所
+    @IBOutlet weak var submitTextEdit: UITextField!
+    
+    ///tableViewをOutlet接続
+    @IBOutlet weak var tableView: UITableView!
+    
+    ///セルに表示するテキスト
+    let texts = ["あ","い","う","え","お","か","き"]
+
+    ///まず最初にここが呼ばれます
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    ///必要なTableViewのメソッド
+    ///セルの行数追加
+    func tableView(_ tableView:UITableView,numberOfRowsInSection section: Int) -> Int {
+        return texts.count
+    }
+    
+    ///必要なTableViewメソッド
+    ///セルのテキスト追加
+    func tableView(_ tableView:UITableView,cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.subtitle, reuseIdentifier: "Cell")
+        
+        cell.textLabel!.text = texts[indexPath.row]
+        
+        return cell
+    }
+    
+    ///セルをタップしたときのクリックイベント
+    func tableView(_ tableView: UITableView, didSelectRowAtIndexPath IndexPath:NSIndexPath) {
+        print(texts[IndexPath.row])
+    }
+    
+    ///登録ボタンをクリックしたときのクリックイベント
+    @IBAction func addText(_ sender: AnyObject) {
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
